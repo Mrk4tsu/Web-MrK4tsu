21c31584e2c31be7f5ae7e544635d8023a44f2a9
MrKatsu <mrk4tsu@gmail.com>
2024-06-13 10:37:19 +0700

    Login Func

---
+++ WebMrKatsu/Areas/Admin/Data/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMrKatsu.Areas.Admin.Data
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Mời nhập user name")]
+        public string UserName { set; get; }
+
+        [Required(ErrorMessage = "Mời nhập password")]
+        public string Password { set; get; }
+    }
+}
